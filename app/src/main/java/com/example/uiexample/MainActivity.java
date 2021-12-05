eb68d67118f8fab5f8e9d7a75f197a231f814c02
Shatha928 <439200928@student.ksu.edu.sa>
2021-12-05 09:39:36 +0300

    added button that when you click it, it will change the text view. as simple as that

--- app/src/main/java/com/example/uiexample/MainActivity.java
+++ app/src/main/java/com/example/uiexample/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.uiexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    //part 1
+    public void onbtnXClick(View view){
+        TextView hi = findViewById(R.id.helloTxt);
+        hi.setText("I Love you!");
     }
 }
