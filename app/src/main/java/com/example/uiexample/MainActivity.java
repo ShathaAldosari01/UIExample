45f59ad4c456af7303383158937f86fb4c1cc13a
Shatha928 <439200928@student.ksu.edu.sa>
2021-12-05 21:43:06 +0300

    simple todo list. no database. when check show Toast, Also when delete show Toast. note it does not delete it just show Toast.

--- app/src/main/java/com/example/uiexample/MainActivity.java
+++ app/src/main/java/com/example/uiexample/MainActivity.java
@@ -3,21 +3,100 @@ package com.example.uiexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnHoverListener, CompoundButton.OnCheckedChangeListener {
+
+    private CheckBox tasks[]= new CheckBox[3];
+    private TextView deletes[] = new TextView[3];
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.deleteT1:
+            case R.id.deleteT2:
+            case R.id.deleteT3:
+                Toast.makeText(this, "Tast Deleted!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        switch (compoundButton.getId()){
+            case R.id.task1:
+            case R.id.task2:
+            case R.id.task3:
+                if(b)
+                    Toast.makeText(this, "Will Done!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public boolean onHover(View view, MotionEvent motionEvent) {
+        Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-    //part 1
-    public void onbtnXClick(View view){
-        TextView hi = findViewById(R.id.helloTxt);
-        hi.setText("I Love you!");
+        tasks[0] = findViewById(R.id.task1);
+        tasks[1] = findViewById(R.id.task2);
+        tasks[2] = findViewById(R.id.task3);
+
+        deletes[0] = findViewById(R.id.deleteT1);
+        deletes[1] = findViewById(R.id.deleteT2);
+        deletes[2] = findViewById(R.id.deleteT3);
+
+        tasks[0].setOnCheckedChangeListener((CompoundButton.OnCheckedChangeListener) this);
+        tasks[1].setOnCheckedChangeListener((CompoundButton.OnCheckedChangeListener) this);
+        tasks[2].setOnCheckedChangeListener((CompoundButton.OnCheckedChangeListener) this);
+
+        deletes[0].setOnClickListener(this);
+        deletes[1].setOnClickListener(this);
+        deletes[2].setOnClickListener(this);
+
+
+//       //other way yo do it
+//        tasks[0].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+//                if(b)
+//                    Toast.makeText(MainActivity.this, "Will Done!", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        tasks[1].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+//                if(b)
+//                    Toast.makeText(MainActivity.this, "Will Done!", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        tasks[2].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+//                if(b)
+//                    Toast.makeText(MainActivity.this, "Will Done!", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+
+//        /*I can know if it is cheak or not by the following */
+//        tasks[0].isChecked();
+
+
+
     }
 }
